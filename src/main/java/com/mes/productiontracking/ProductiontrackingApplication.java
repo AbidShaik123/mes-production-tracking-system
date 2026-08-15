aa3c34ccbc17cb6311af3a6cfece5acb187d0c62
ABID SHAIK <abidshaikus@gmail.com>
2026-08-15 14:00:59 -0500

    Update ProductiontrackingApplication.java

--- src/main/java/com/mes/productiontracking/ProductiontrackingApplication.java
+++ src/main/java/com/mes/productiontracking/ProductiontrackingApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductiontrackingApplication {
+public class ProductiontrackingApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductiontrackingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MesProductionTrackingSystemApplication.class, args);
+    }
 }
