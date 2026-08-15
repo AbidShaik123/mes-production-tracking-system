a777ff49789901295dd2e46cc7ae70da86e43b0e
ABID SHAIK <abidshaikus@gmail.com>
2026-08-15 13:58:04 -0500

    Add files via upload

---
+++ src/test/java/com/mes/productiontracking/ProductiontrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.productiontracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductiontrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
